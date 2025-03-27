1a1509e64ed897bbea01a84b09ca90c2a9b95e6b
Aayush Dixit <aayush@Aayushs-MacBook-Pro.local>
2025-03-27 20:47:00 +0530

    Fixed Serverside Rendering

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -146,7 +146,7 @@ const Dashboard = () => {
                                         display: true,
                                         text: "Bar Chart",
                                     },
-                                },
+                                }, 
                             }}
                         />
                     </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,19 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["300", "400", "700"], // Add required weights
 });
 
 export const metadata = {
@@ -20,7 +25,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         {children}
       </body>
